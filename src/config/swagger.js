fc7e556074c8e916cb23cd754e8d62a4eaae873d
Hieunmtse150772 <nguyenminhtrunghieu10@gmail.com>
2024-06-24 21:06:20 +0700

    #7 - feat: fix bug swagger, auth

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -7,42 +7,41 @@ const userSwagger = require('../swaggers/user.swagger');
 const friendSwagger = require('../swaggers/friend.swagger');
 const conversationSwagger = require('../swaggers/conversation.swagger');
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: package.name,
-      version: package.version,
-      description: package.description,
-    },
-    servers: [
-      {
-        url: process.env.URL_API,
-        description: process.env.ENVIRONMENT,
-      },
-    ],
-    tags: ['Auth', 'Message', 'User'],
-    paths: {
-      ...authSwagger,
-      ...messageSwagger,
-      ...userSwagger,
-      ...conversationSwagger,
-      ...friendSwagger,
-    },
-    components: {
-      /* ... */
-      securitySchemes: {
-        accessToken: {
-          type: 'http',
-          scheme: 'bearer',
-          in: 'header',
-          bearerFormat: 'JWT',
-          description:
-            "Enter the token don't need the `Bearer: ` prefix, e.g. 'abcde12345'.",
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: package.name,
+            version: package.version,
+            description: package.description,
         },
-      },
+        servers: [
+            {
+                url: process.env.URL_API,
+                description: process.env.ENVIRONMENT,
+            },
+        ],
+        tags: ['Auth', 'Message', 'User'],
+        paths: {
+            ...authSwagger,
+            ...messageSwagger,
+            ...userSwagger,
+            ...conversationSwagger,
+            ...friendSwagger,
+        },
+        components: {
+            /* ... */
+            securitySchemes: {
+                accessToken: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    in: 'header',
+                    bearerFormat: 'JWT',
+                    description: "Enter the token don't need the `Bearer: ` prefix, e.g. 'abcde12345'.",
+                },
+            },
+        },
     },
-  },
-  apis: ['./routes/*.js'],
+    apis: ['./routes/*.js'],
 };
 
 module.exports = swaggerJsdoc(options);
