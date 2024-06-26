41f47f1f35cd795efb86c84863bfdae29be0e97d
MaiLinhChi <mailinhchi22082002@gmail.com>
2024-06-26 15:55:04 +0700

    fix: delete index.html

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -5,9 +5,6 @@ const verifyToken = require('../middleware/verifyToken.middleware');
 const Validation = require('../middleware/validation.middleware');
 const { getMessages, sendMessage } = require('../validations/message.validation');
 
-router.get('/chat', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
 router.get('/:id', verifyToken, Validation(getMessages), MessageController.getMessages);
 router.post('/send/:id', verifyToken, Validation(sendMessage), MessageController.sendMessage);
 
