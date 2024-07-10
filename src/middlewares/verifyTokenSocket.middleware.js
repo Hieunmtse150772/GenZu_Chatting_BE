3413c350ca74b59f605c624be0c7fe8c43e81f42
MaiLinhChi <mailinhchi22082002@gmail.com>
2024-07-10 09:05:06 +0700

    fix: await socket

--- src/middlewares/verifyTokenSocket.middleware.js
+++ src/middlewares/verifyTokenSocket.middleware.js
@@ -4,20 +4,29 @@ const createResponse = require('@/utils/responseHelper');
 const User = require('@/model/user.model');
 const { STATUS_MESSAGE, MESSAGE_CODE, STATUS_CODE } = require('@/enums/response');
 
-module.exports = async function (token, socket) {
+module.exports = function (token, socket) {
+    let isError = true;
+
     try {
         if (!token) {
-            return socket.emit(
+            socket.emit(
                 'validation',
-                createResponse(null, STATUS_MESSAGE.UNAUTHORIED, MESSAGE_CODE.UNAUTHORIED, STATUS_CODE.UNAUTHORIED),
+                createResponse(
+                    null,
+                    STATUS_MESSAGE.UNAUTHORIED,
+                    MESSAGE_CODE.UNAUTHORIED,
+                    STATUS_CODE.UNAUTHORIZED,
+                    false,
+                ),
             );
+            return isError;
         }
 
         const accessToken = token.split(' ')[1];
         const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_KEY);
 
         if (!decoded) {
-            return socket.emit(
+            socket.emit(
                 'validation',
                 createResponse(
                     null,
@@ -27,42 +36,49 @@ module.exports = async function (token, socket) {
                     false,
                 ),
             );
+            return isError;
         }
 
-        const user = await User.findById(decoded.data).select('-password');
-        if (!user) {
-            return socket.emit(
-                'validation',
-                createResponse(
-                    null,
-                    STATUS_MESSAGE.USER_NOT_REGISTERED,
-                    MESSAGE_CODE.USER_NOT_REGISTERED,
-                    STATUS_CODE.NOT_FOUND,
-                    false,
-                ),
-            );
-        }
+        User.findById(decoded.data)
+            .select('-password')
+            .then((user) => {
+                if (!user) {
+                    socket.emit(
+                        'validation',
+                        createResponse(
+                            null,
+                            STATUS_MESSAGE.USER_NOT_REGISTERED,
+                            MESSAGE_CODE.USER_NOT_REGISTERED,
+                            STATUS_CODE.NOT_FOUND,
+                            false,
+                        ),
+                    );
+                    return isError;
+                }
 
-        if (!user.is_active) {
-            return socket.emit(
-                'validation',
-                createResponse(
-                    null,
-                    STATUS_MESSAGE.ACCOUNT_INACTIVE,
-                    MESSAGE_CODE.ACCOUNT_INACTIVE,
-                    STATUS_CODE.FORBIDDEN,
-                    false,
-                ),
-            );
-        }
+                if (!user.is_active) {
+                    socket.emit(
+                        'validation',
+                        createResponse(
+                            null,
+                            STATUS_MESSAGE.ACCOUNT_INACTIVE,
+                            MESSAGE_CODE.ACCOUNT_INACTIVE,
+                            STATUS_CODE.FORBIDDEN,
+                            false,
+                        ),
+                    );
+                    return isError;
+                }
 
-        user.socketId.push(socket.id);
-        user.is_online = true;
-        const newUser = await user.save();
-
-        socket.user = newUser;
-        return false;
+                user.socketId.push(socket.id);
+                user.is_online = true;
+                user.save().then((newUser) => {
+                    socket.user = newUser;
+                    return false;
+                });
+            });
     } catch (err) {
-        return socket.emit('validation', createResponse(err, null, null, STATUS_CODE.UNAUTHORIZED, false));
+        socket.emit('validation', createResponse(err, null, null, STATUS_CODE.UNAUTHORIZED, false));
+        return isError;
     }
 };
